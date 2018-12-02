0e199ff743f80888d9346dc20aaddf12c944e450
yoat <yoatnic@me.com>
2018-12-02 17:15:41 +0900

    add bolierplate

---
+++ Dockerfile
@@ -0,0 +1,1 @@
+FROM wordpress:4.9.8
---
+++ my-theme/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  <body>
+    <header>header</header>
